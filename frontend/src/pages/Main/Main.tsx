730f09fe5a9c1555287949e625e8ac3caf63b1cf
Mirim Park <mirim920@naver.com>
2022-11-15 19:19:40 +0900

    ✨ feat: popular 컴포넌트 KeywordRanking으로 이름 수정, 인기검색어 드롭다운 구현

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -1,66 +1,121 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import PrvFooter from '../../components/PrvFooter';
 import PrvLogo from '../../components/PrvLogo';
 import PrvSearch from '../../components/PrvSearch';
 import { SUBTITLE, TITLE, TITLE_KOREAN } from '../../constants/main';
-import Popular from './components/TopSearched';
+import RankingList from './components/RankingList';
+import RankingSlide from './components/RankingSlide';
+
+interface IRankingData {
+  keyword: string;
+  count: number;
+}
 
 const Main = () => {
-	return (
-		<Container>
-			<MainContainer>
-				<TitleContainer>
-					<PrvLogo />
-					<Title>PRV</Title>
-				</TitleContainer>
-				<ContentContainer>
-					<div>{TITLE}</div>
-					<div>{TITLE_KOREAN}</div>
-					<div>{SUBTITLE}</div>
-				</ContentContainer>
-				<Popular />
-				<PrvSearch />
-			</MainContainer>
-			<PrvFooter />
-		</Container>
-	);
+  const [isRankingListOpen, setisRankingListOpen] = useState(false);
+  const [rankingData, setRankingData] = useState<IRankingData[]>([]);
+  const openRankingList = () => {
+    setisRankingListOpen(true);
+  };
+  const closeRankingList = () => {
+    setisRankingListOpen(false);
+  };
+
+  useEffect(() => {
+    const fetchKeywordRanking = async () => {
+      const response = await fetch('/data/keywordRanking.json');
+      const data: IRankingData[] = await response.json();
+      console.log(data);
+      setRankingData(data);
+    };
+
+    fetchKeywordRanking();
+  }, []);
+
+  return (
+    <Container>
+      <MainContainer>
+        <TitleContainer>
+          <PrvLogo />
+          <Title>PRV</Title>
+        </TitleContainer>
+        <ContentContainer>
+          <div>{TITLE}</div>
+          <div>{TITLE_KOREAN}</div>
+          <div>{SUBTITLE}</div>
+        </ContentContainer>
+        <RankingContainer>
+          <RankingBar>
+            {isRankingListOpen ? (
+              <RankingList rankingData={rankingData} handleButtonClick={closeRankingList} />
+            ) : (
+              <RankingSlide rankingData={rankingData} handleButtonClick={openRankingList} />
+            )}
+          </RankingBar>
+        </RankingContainer>
+        <PrvSearch />
+      </MainContainer>
+      <PrvFooter />
+    </Container>
+  );
 };
 
 const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	align-items: center;
-	height: 100%;
-	background-color: ${({ theme }) => theme.COLOR.primary3};
-	color: ${({ theme }) => theme.COLOR.offWhite};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  background-color: ${({ theme }) => theme.COLOR.primary3};
+  color: ${({ theme }) => theme.COLOR.offWhite};
 `;
 
 const MainContainer = styled.main`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: 10px;
-	height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  height: 100%;
 `;
 
 const TitleContainer = styled.div`
-	display: flex;
-	align-items: center;
-	gap: 10px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
 `;
 
 const Title = styled.div`
-	${({ theme }) => theme.TYPO.H4};
+  ${({ theme }) => theme.TYPO.H4};
 `;
 
 const ContentContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 10px;
-	${({ theme }) => theme.TYPO.body2};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  ${({ theme }) => theme.TYPO.body2};
+`;
+
+const RankingContainer = styled.div`
+  position: relative;
+  width: 500px;
+  height: 70px;
+`;
+
+const RankingBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  margin-top: 30px;
+  padding: 10px 20px;
+  background-color: ${({ theme }) => theme.COLOR.primary3};
+  border: 1px solid ${({ theme }) => theme.COLOR.offWhite};
+  border-radius: 20px;
+  z-index: 10;
 `;
 
 export default Main;
