f080ef28aa37148e6acb7721f0788405c389bd13
yongbul <top1506@naver.com>
2022-11-15 11:28:24 +0900

    refactor: redis 연결과 redist 접근 key 환경변수로 설정

--- backend/src/popular/popular.service.ts
+++ backend/src/popular/popular.service.ts
@@ -6,10 +6,10 @@ import Redis from 'ioredis';
 export class PopularService {
 	constructor(@InjectRedis() private readonly redis: Redis) {}
 	async getAll() {
-		const topTen = await this.redis.zrevrangebyscore('Top10', '+inf', 1);
+		const topTen = await this.redis.zrevrangebyscore(process.env.REDIS_POPULAR_KEY, '+inf', 1);
 		const scores = await Promise.all(
 			topTen.map((v) => {
-				return this.redis.zscore('Top10', v);
+				return this.redis.zscore(process.env.REDIS_POPULAR_KEY, v);
 			}),
 		);
 		return scores.map((score, i) => {
@@ -18,7 +18,7 @@ export class PopularService {
 		});
 	}
 	async insertRedis(data: string) {
-		const isRanking: string = await this.redis.zscore('Top10', data);
+		const isRanking: string = await this.redis.zscore(process.env.REDIS_POPULAR_KEY, data);
 		isRanking ? await this.redis.zadd('Top10', Number(isRanking) + 1, data) : await this.redis.zadd('Top10', 1, data);
 	}
 }
