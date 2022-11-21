3b31d4bc3bb876397e6e1bf9df58dcdaad77eb5e
yeynii <yeynii@gmail.com>
2022-11-21 13:55:04 +0900

    8-1 [FE] [메인 - 로딩] 자동완성 검색 응답 대기시 스피너(달) 을 보여준다, 5-1-[refactor] (#36)

---
+++ frontend/src/components/AutoCompletedList.tsx
@@ -0,0 +1,103 @@
+import { Dispatch, SetStateAction, useEffect } from 'react';
+import styled from 'styled-components';
+import { IAutoCompletedItem } from './Search';
+
+interface AutoCompletedListProps {
+  autoCompletedItems: IAutoCompletedItem[];
+  keyword: string;
+  hoverdIndex: number;
+  setHoveredIndex: Dispatch<SetStateAction<number>>;
+}
+
+const AutoCompletedList = ({ autoCompletedItems, keyword, hoverdIndex, setHoveredIndex }: AutoCompletedListProps) => {
+  const handleAutoCompletedDown = (index: number) => {
+    // Todo : 상세정보 api 호출
+    console.log('상세정보', autoCompletedItems[index].doi);
+  };
+
+  // keyword 강조
+  const highlightKeyword = (text: string) => {
+    const rawKeyword = keyword.trim().toLowerCase();
+    return rawKeyword !== '' && text.toLowerCase().includes(rawKeyword)
+      ? text
+          .split(new RegExp(`(${rawKeyword})`, 'gi'))
+          .map((part, i) => (part.trim().toLowerCase() === rawKeyword ? <Emphasize key={i}>{part}</Emphasize> : part))
+      : text;
+  };
+
+  // 대표 author찾기
+  const getRepresentativeAuthor = (authors: string[]) => {
+    return (
+      authors
+        .concat()
+        .filter((v, i, arr: string[]) => v.toLowerCase().includes(keyword.toLowerCase()) && arr.splice(i))?.[0] ||
+      authors[0]
+    );
+  };
+
+  useEffect(() => {
+    setHoveredIndex(-1);
+  }, [setHoveredIndex]);
+
+  return (
+    <Container>
+      {autoCompletedItems.length > 0 ? (
+        autoCompletedItems.map((item, i) => (
+          <AutoCompleted
+            key={item.doi}
+            hovered={i === hoverdIndex}
+            onMouseOver={() => setHoveredIndex(i)}
+            onMouseDown={() => handleAutoCompletedDown(i)}
+          >
+            <Title>{highlightKeyword(item.title)}</Title>
+            {item.authors && (
+              <Author>
+                authors : {highlightKeyword(getRepresentativeAuthor(item.authors))}
+                {item.authors.length > 1 && <span>외 {item.authors.length - 1}명</span>}
+              </Author>
+            )}
+          </AutoCompleted>
+        ))
+      ) : (
+        <NoResult>자동완성 검색어가 없습니다.</NoResult>
+      )}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  overflow-y: auto;
+`;
+
+const AutoCompleted = styled.li<{ hovered: boolean }>`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 8px 30px;
+  gap: 4px;
+  color: ${({ theme }) => theme.COLOR.black};
+  cursor: pointer;
+  background-color: ${({ theme, hovered }) => (hovered ? theme.COLOR.gray1 : 'auto')};
+`;
+
+const Title = styled.div`
+  ${({ theme }) => theme.TYPO.body1}
+`;
+
+const Author = styled.div`
+  ${({ theme }) => theme.TYPO.caption}
+  color: ${({ theme }) => theme.COLOR.gray3};
+`;
+
+const Emphasize = styled.span`
+  color: #3244ff;
+  font-weight: 700;
+`;
+
+const NoResult = styled.div`
+  padding-top: 25px;
+  text-align: center;
+  overflow: hidden;
+`;
+
+export default AutoCompletedList;
---
+++ frontend/src/components/MoonLoader.tsx
@@ -0,0 +1,62 @@
+import * as d3 from 'd3';
+import { memo, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import theme from '../style/theme';
+
+const MoonLoader = () => {
+  const pathRef = useRef<SVGPathElement>(null);
+  const moonSize = 50;
+
+  const calculateMoonLightPath = (radian: number) => {
+    return `M 50,0
+    A 50,50 0 0 ${Math.floor(radian / Math.PI) % 2} 50,100
+    A ${50 * Math.cos(radian)},50 0 0 ${Math.floor((radian / Math.PI) * 2) % 2} 50,0`;
+  };
+
+  useEffect(() => {
+    let radian = Math.PI;
+    const timer = d3.interval(() => {
+      d3.select(pathRef.current).attr('d', calculateMoonLightPath(radian));
+      radian += Math.PI / 10;
+    }, 100);
+    return () => timer.stop();
+  }, []);
+
+  return (
+    <MoonContainer>
+      <svg width={moonSize} height={moonSize} viewBox="0 0 100 100" style={{ filter: 'url(#inset-shadow)' }}>
+        <defs>
+          <filter id="inset-shadow" x="-50%" y="-50%" width="200%" height="200%">
+            <feComponentTransfer in="SourceAlpha">
+              <feFuncA type="table" tableValues="1 0" />
+            </feComponentTransfer>
+            <feGaussianBlur stdDeviation="3" />
+            <feOffset dx="0" dy="0" result="offsetblur" />
+            <feFlood floodColor="rgb(0, 0, 0, 0.7)" result="color" />
+            <feComposite in2="offsetblur" operator="in" />
+            <feComposite in2="SourceAlpha" operator="in" />
+            <feMerge>
+              <feMergeNode in="SourceGraphic" />
+              <feMergeNode />
+            </feMerge>
+          </filter>
+          <pattern id="image11" x="0" y="0" patternUnits="userSpaceOnUse" height="100" width="100">
+            <image x="0" y="0" height="100" width="100" xlinkHref="assets/moon.png"></image>
+          </pattern>
+        </defs>
+        <g>
+          <circle cx="50" cy="50" r="49" stroke="0" fill={theme.COLOR.primary4} />
+          <path ref={pathRef} fill={theme.COLOR.offWhite} />
+          <circle cx="50" cy="50" r="50" strokeWidth="0" fill="url(#image11)" />
+        </g>
+      </svg>
+    </MoonContainer>
+  );
+};
+
+const MoonContainer = styled.div`
+  text-align: center;
+  margin-top: 25px;
+`;
+
+export default memo(MoonLoader);
