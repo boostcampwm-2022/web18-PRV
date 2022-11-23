9dcc5540ed81369bb764d4a5f7e60d8e50cd57a4
Mirim Park <50133823+Palwol@users.noreply.github.com>
2022-11-23 13:21:32 +0900

    7-9 [FE] [fix] 검색어 자동완성 api 오류 시 재요청이 되지 않던 오류 수정 (#53)

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 export interface IGetSearch {
   keyword?: string;
@@ -10,42 +10,12 @@ export interface IGetAutoComplete {
   keyword: string;
 }
 
-const MAX_RETRY_COUNT = 5;
-
-const customAxiosInstance = (baseURL: string) => {
-  const axiosInstance = axios.create({
-    baseURL,
-  });
-  const onFulfilled = (response: AxiosResponse) => response;
-  let retryCount = 0;
-
-  const retry = (errorConfig: AxiosRequestConfig<unknown>) => {
-    retryCount += 1;
-    if (retryCount > MAX_RETRY_COUNT) {
-      throw new Error('MAX_RETRY_EXCEEDED');
-    }
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(axiosInstance.request(errorConfig));
-      });
-    });
-  };
-  const onRejected = (error: { config: AxiosRequestConfig<unknown> }) => {
-    if (error.config) {
-      return retry(error.config);
-    }
-    return Promise.reject(error);
-  };
-  axiosInstance.interceptors.response.use(onFulfilled, onRejected);
-  return axiosInstance;
-};
-
 export default class Api {
-  private readonly baseURL = 'http://49.50.172.204:4000';
+  private readonly baseURL = 'http://49.50.172.204:4000/';
   private readonly instance: AxiosInstance;
 
   constructor() {
-    this.instance = customAxiosInstance(this.baseURL);
+    this.instance = axios.create({ baseURL: this.baseURL });
   }
 
   getKeywordRanking() {
