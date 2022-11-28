23d55d4923bf85b8ff414c4256c6531f4caa9bef
yongbul <top1506@naver.com>
2022-11-28 14:09:23 +0900

    chore: conflict 해결

--- backend/src/util.ts
+++ backend/src/util.ts
@@ -1,7 +1,8 @@
+const BASE_URL = 'https://api.crossref.org/works';
+
 export const CROSSREF_API_URL = (keyword: string, rows = 5, selects: string[] = ['author', 'title', 'DOI'], page = 1) =>
-  `https://api.crossref.org/works?query=${keyword}&rows=${rows}&select=${selects.join(',')}&offset=${
-    rows * (page - 1)
-  }`;
+  `${BASE_URL}?query=${keyword}&rows=${rows}&select=${selects.join(',')}&offset=${rows * (page - 1)}`;
+export const CROSSREF_API_PAPER_URL = (doi: string) => `${BASE_URL}/${doi}`;
 class Queue {
   data: Set<string>;
   constructor() {
