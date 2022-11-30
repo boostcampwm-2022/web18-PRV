b067c4a83261c17d6a25b2218831c5e009a0aa12
Mirim Park <mirim920@naver.com>
2022-12-01 00:00:52 +0900

    ✨ feat: 에러 페이지 구현

--- frontend/src/error/GlobalErrorFallback.tsx
+++ frontend/src/error/GlobalErrorFallback.tsx
@@ -11,9 +11,10 @@ const GlobalErrorFallback = ({ error }: IProps) => {
 
   return (
     <Container>
-      <h1>{code || 404} Error</h1>
+      <H1>Oops!</H1>
+      <H2>{code || 404} Error occurred</H2>
       <Link to="/">
-        <button>return to home</button>
+        <Button>Go Home</Button>
       </Link>
     </Container>
   );
@@ -21,9 +22,32 @@ const GlobalErrorFallback = ({ error }: IProps) => {
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
   justify-content: center;
+  align-items: center;
+  gap: 30px;
+  background-color: ${({ theme }) => theme.COLOR.primary3};
+  color: ${({ theme }) => theme.COLOR.offWhite};
+`;
+
+const H1 = styled.h1`
+  font-size: 70px;
+  font-weight: 700;
+  margin-bottom: 35px;
+`;
+
+const H2 = styled.h2`
+  ${({ theme }) => theme.TYPO.title}
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  color: ${({ theme }) => theme.COLOR.offWhite};
+  background-color: ${({ theme }) => theme.COLOR.primary2};
+  border-radius: 20px;
+  cursor: pointer;
 `;
 
 export default GlobalErrorFallback;
