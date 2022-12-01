e75c747d5b8c8458066a32112594d3472d3144e4
yongbul <top1506@naver.com>
2022-12-01 22:17:33 +0900

    feat: insert redis 빈 검색어 삽입시 에러 처리 추가

--- backend/src/ranking/tests/ranking.controller.spec.ts
+++ backend/src/ranking/tests/ranking.controller.spec.ts
@@ -1,5 +1,5 @@
 import { RedisModule } from '@liaoliaots/nestjs-redis';
-import { INestApplication } from '@nestjs/common';
+import { BadRequestException, INestApplication } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { RankingController } from '../ranking.controller';
@@ -17,23 +17,28 @@ describe('RankingServiceTest', () => {
     controller = module.get<RankingController>(RankingController);
     service = module.get<RankingService>(RankingService);
   });
-  describe('/keyword-ranking/insert', () => {
-    //Case1. 기존 redis에 없던 데이터 삽입
-    // it('검색어 redis에 삽입', async () => {
-    //   const result = await controller.insertCache('newData');
-    //   expect(result).toBe(true);
-    // });
-    //Case1. 기존 redis에 있던 데이터 삽입
-    it('검색어 redis에 삽입', async () => {
-      const result = await controller.insertCache('부스트캠프');
-      expect(result).toBe('update');
-    });
-  });
   describe('/keyword-ranking', () => {
     it('10위까지의 검색어를 가져오기', async () => {
       //Case 1. redis date가 10개 이하인 경우
       const topTen = await controller.getTen();
       expect(topTen.length).toBeLessThanOrEqual(10);
+    });
+  });
+  describe('/keyword-ranking/insert', () => {
+    // Case1. 기존 redis에 없던 데이터 삽입
+    it('검색어 redis에 삽입', async () => {
+      const result = await controller.insertCache('newData');
+      expect(result).toBe('new');
+    });
+    // Case2. 기존 redis에 있던 데이터 삽입
+    it('검색어 redis에 삽입', async () => {
+      const result = await controller.insertCache('부스트캠프');
+      expect(result).toBe('update');
+    });
+    it('빈 검색어 redis에 삽입', async () => {
+      await expect(controller.insertCache('')).rejects.toEqual(
+        new BadRequestException({ status: 400, error: 'bad request' }),
+      );
     });
   });
 });
--- backend/src/ranking/tests/ranking.service.mock.ts
+++ backend/src/ranking/tests/ranking.service.mock.ts
@@ -1,19 +1,16 @@
+import { BadRequestException } from '@nestjs/common';
 import { redisRanking } from '../entities/ranking.entity';
 import redisMockData from './rankingData.mock';
 export function mockRedisService() {
   const insertRedis = jest.fn(async (keyword: string) => {
-    // return redisMockData.reduce((acc, select) => {
-    //   if (select.keyword === keyword) {
-    //     select[keyword]++;
-    //     return true;
-    //   }
-    // }, {});
+    if (keyword === '') throw new BadRequestException({ status: 400, error: 'bad request' });
     for (const data of redisMockData) {
       if (data.keyword === keyword) {
         data[keyword]++;
-        return true;
+        return 'update';
       }
     }
+    return 'new';
   });
   const getTen = jest.fn().mockResolvedValue(redisMockData.slice(0, 10));
   const redisService = { insertRedis, getTen };
