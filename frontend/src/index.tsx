6ca9451a5399246c0505af900de6c1f2f89e1033
yeynii <yeynii@gmail.com>
2022-12-08 16:09:30 +0900

    revert: react.strictmode 되돌리기

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -5,7 +6,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
