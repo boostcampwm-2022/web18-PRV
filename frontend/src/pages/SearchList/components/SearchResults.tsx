b089da9345167aea528dbd84adb8a00ff6a59c66
Mirim Park <mirim920@naver.com>
2022-12-15 14:44:25 +0900

    ✨ feat: 검색결과 페이지 데이터 안내 추가

--- frontend/src/pages/SearchList/components/SearchResults.tsx
+++ frontend/src/pages/SearchList/components/SearchResults.tsx
@@ -1,7 +1,10 @@
 import { IGetSearch } from '@/api/api';
-import { Pagination } from '@/components';
+import { IconButton, Pagination } from '@/components';
+import InfoIcon from '@/icons/InfoIcon';
 import { useSearchQuery } from '@/queries/queries';
+import theme from '@/style/theme';
 import { createDetailQuery } from '@/utils/createQueryString';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Paper from './Paper';
@@ -12,13 +15,28 @@ interface SearchResultsProps {
 }
 
 const SearchResults = ({ params, changePage }: SearchResultsProps) => {
+  const [isTooltipOpened, setIsTooltipOpened] = useState(false);
   const keyword = params.keyword || '';
   const page = Number(params.page);
   const { data } = useSearchQuery(params);
 
+  const handleMouseOver = () => {
+    setIsTooltipOpened(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsTooltipOpened(false);
+  };
+
   return data && data.papers.length > 0 ? (
     <>
-      <H1>Articles ({data.pageInfo.totalItems.toLocaleString() || 0})</H1>
+      <SectionHeader>
+        <H1>Articles ({data.pageInfo.totalItems.toLocaleString() || 0})</H1>
+        <IconButtonWrapper onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+          <IconButton icon={<InfoIcon color={theme.COLOR.gray3} />} aria-label="정보" />
+        </IconButtonWrapper>
+        {isTooltipOpened && <InfoTooltip>논문의 정보가 정확하지 않거나 누락되어 있을 수 있습니다.</InfoTooltip>}
+      </SectionHeader>
       <Hr />
       <Section>
         <Papers>
@@ -36,10 +54,32 @@ const SearchResults = ({ params, changePage }: SearchResultsProps) => {
   );
 };
 
+const SectionHeader = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const H1 = styled.h1`
   color: ${({ theme }) => theme.COLOR.gray4};
-  margin: 16px 30px;
+  margin: 16px 15px 16px 30px;
   ${({ theme }) => theme.TYPO.H5}
+`;
+
+const IconButtonWrapper = styled.div`
+  opacity: 0.5;
+  cursor: pointer;
+  z-index: 10;
+  :hover {
+    opacity: 1;
+  }
+`;
+
+const InfoTooltip = styled.span`
+  ${({ theme }) => theme.TYPO.body1};
+  font-weight: 700;
+  padding: 5px 8px;
+  margin-left: 10px;
+  color: ${({ theme }) => theme.COLOR.gray4};
 `;
 
 const Hr = styled.hr`
